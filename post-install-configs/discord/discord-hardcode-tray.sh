cb2a952328b3ba1b2ac8181120421d897de0545b
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2021-10-23 14:38:20 +0200

    Añadida alguna cosa que no sé qué es

--- post-install-configs/discord/discord-hardcode-tray.sh
+++ post-install-configs/discord/discord-hardcode-tray.sh
@@ -4,24 +4,25 @@
 ## FECHA DE CREACIÓN: Desconocida
 ## FECHA DE MODIFICACIÓN: 23 de octubre de 2020, 5 de marzo de 2021
 
-ROOTDIR=$(realpath $(dirname $0)/..)
-DISCORDVERSION=0.0.15
+ROOTDIR=$(realpath $(dirname $0))
 
 #if [[ -d "/opt/Discord" ]]; then
 if dpkg --get-selections | grep discord; then
+	DISCORDVERSION=$(dpkg -s discord | grep '^Version:' | cut -c 10-)
 	if ! dpkg --get-selections | grep npm; then
 		sudo apt install -y npm
 		INSTALADO=1
 	fi
 	sudo npm install -g asar
 	asar e $HOME/.config/discord/$DISCORDVERSION/modules/discord_desktop_core/core.asar $HOME/.config/discord/$DISCORDVERSION/modules/discord_desktop_core/core
-	echo WAIT
-	read
-	for i in $ROOTDIR/files/discord-hardcode-tray/*.png; do
+	if [[ $1 == "--light" ]]; then
+		CARPETA=$ROOTDIR/discord-hardcode-tray-light
+	else
+		CARPETA=$ROOTDIR/discord-hardcode-tray
+	fi
+	for i in $CARPETA/*.png; do
 		cp -rv "$(realpath $i)" "$HOME/.config/discord/$DISCORDVERSION/modules/discord_desktop_core/core/app/images/systemtray/linux/$(basename $i)"
 	done
-	echo wait
-	read
 	asar p $HOME/.config/discord/$DISCORDVERSION/modules/discord_desktop_core/core $HOME/.config/discord/$DISCORDVERSION/modules/discord_desktop_core/core.asar
 	rm -rfv $HOME/.config/discord/$DISCORDVERSION/modules/discord_desktop_core/core
 	sudo npm uninstall -g asar
